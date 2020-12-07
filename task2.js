803d95d46f57eb7c167e8d9dcd52b6ee85f8079a
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2020-12-07 21:57:49 +0200

    сделал 6 дз

--- task2.js
+++ task2.js
@@ -1,9 +1,5 @@
 
 const total = 100;
-let ordered = 50;
-
-// ordered = 20;
-// ordered = 80;
-// ordered = 130;
+const ordered = 50;
 
 total > ordered ? alert("Заказ оформлен, с вами свяжется менеджер") : alert("На складе недостаточно товаров!")
--- task3.js
+++ task3.js
@@ -1,12 +1,23 @@
+// const ADMIN_PASSWORD = 'jqueryismyjam';
+// let message = prompt('Введите пароль');
+
+// if (message === null) {
+//   message = 'Отменено пользователем'
+// } else if (message === ADMIN_PASSWORD) {
+//   message = 'Добро пожаловать'
+// } else {
+//   message = 'Доступ запрещен неверный пароль!'
+// }
+
+// alert(message)
+
+const userPassword = 'jqueryismyjam';
+
 const ADMIN_PASSWORD = 'jqueryismyjam';
-let message = prompt('Введите пароль');
 
-if (message === null) {
-  message = 'Отменено пользователем'
-} else if (message === ADMIN_PASSWORD) {
-  message = 'Добро пожаловать'
-} else {
-  message = 'Доступ запрещен неверный пароль!'
-}
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_IS_ALLOWED = 'Добро пожаловать!';
+const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!';
+let message;
 
-alert(message)
+// Write code under this line
